e731130baea476dfe811dcc56fe175d9f445be9b
Jaewon Kim <jaewon.kim1326@gmail.com>
2024-07-16 05:28:42 -0500

    added valitdation for layers, now need to add validation for compiler settings

--- src/scripts/TensorflowFunctions.ts
+++ src/scripts/TensorflowFunctions.ts
@@ -1,154 +1,45 @@
 import * as tf from "@tensorflow/tfjs-node";
-import {
-	activationList,
-	Conv2DLayer,
-	DenseLayer,
-	Flatten,
-	kernelInitializerList,
-	lambdaList,
-	Layer,
-	MaxPooling2D,
-	regularizerList,
-} from "./InterfacesAndConsts";
-import { error } from "console";
-
+import { convertLayers } from "./nnscripts";
 //main functions
-export const compileDense = async (layers: any[], loss: string, shape: any): Promise<tf.LayersModel> => {
-	const newLayers: any = layers.map((layer) => ({
-		activation: layer.activation,
-		units: layer.units,
-		kernelRegularizer: regularizerToFunction(layer.regularizer),
-	}));
-	newLayers[0].inputShape = shape;
-	const model = tf.sequential();
-	newLayers.forEach((layer: any, index: any) => {
-		model.add(tf.layers.dense(layer));
-	});
+export const compileModel = async (layers: any[], loss: string) => {
+	try {
+		const layerArr = convertLayers(layers);
+		const model = tf.sequential();
+		if (layers[0].type == "Conv2D") {
+			layers[0].inputShape = [28, 28, 1];
+		}
 
-	model.compile({
-		loss: loss,
-		optimizer: tf.train.adam(0.01),
-		metrics: ["accuracy"],
-	});
+		layerArr?.forEach((layer: any, index: any) => {
+			switch (layer.type) {
+				case "Flatten":
+					model.add(tf.layers.flatten());
+					break;
+				case "MaxPooling2D":
+					model.add(tf.layers.maxPool2d(layer.layer));
+					break;
+				case "Dense":
+					model.add(tf.layers.dense(layer.layer));
+					break;
+				case "Conv2D":
+					model.add(tf.layers.conv2d(layer.layer));
+					break;
+				default:
+					throw new Error("Invaild Layer");
+			}
+		});
 
-	return model;
+		model.compile({
+			loss: loss,
+			optimizer: tf.train.adam(0.01),
+			metrics: ["accuracy"],
+		});
+
+		return model;
+	} catch (error) {}
 };
-
-export const compileConv2d = async () => {};
 
 export const train = async (model: tf.LayersModel, dataName: string, index: number) => {};
 
 export const predict = (data: number[], model: any) => {
 	const tensor = tf.tensor(data);
-};
-
-//helper functions
-function regularizerToFunction(reg: { regularizer: string; lambda: number }) {
-	let regularizerFunction;
-	switch (reg.regularizer) {
-		case "l1":
-			regularizerFunction = tf.regularizers.l1({ l1: reg.lambda });
-			break;
-		case "l2":
-			regularizerFunction = tf.regularizers.l2({ l2: reg.lambda });
-			break;
-		case "l1l2":
-			regularizerFunction = tf.regularizers.l1l2({ l1: reg.lambda, l2: reg.lambda });
-			break;
-	}
-
-	return regularizerFunction;
-}
-
-const convertLayers = (layers: any[]) => {
-	try {
-		const layerCheck: Layer[] = layers;
-		const fixedLayers = layers.map((layerObj, index) => {
-			switch (layerObj.type) {
-				case "Flatten":
-					validateFlatten(layerObj.layer);
-					return layerObj.layer;
-				case "MaxPooling2D":
-					validateMaxPooling2D(layerObj.layer);
-					return layerObj.layer;
-				case "Dense":
-					validateDense(layerObj.layer);
-					const copy: any = { ...layerObj };
-					copy.layer.kernelRegularizer = regularizerToFunction(copy.layer.kernelRegularizer);
-					return copy.layer;
-				case "Conv2D":
-					validateConv2D(layerObj.layer);
-					const copy2: any = { ...layerObj };
-					copy2.layer.kernelRegularizer = regularizerToFunction(copy2.layer.kernelRegularizer);
-					return copy.layer;
-				default:
-					throw new Error("Invaild Layer");
-			}
-		});
-		return fixedLayers;
-	} catch (error) {
-		//here instead save it to an error log
-	}
-};
-
-const validateDense = (layer: any) => {
-	try {
-		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild Units");
-		if (activationList.find((element) => element == layer.activation) == undefined) {
-			throw new Error("Invaild Activation");
-		}
-		if (regularizerList.find((element) => element == layer.kernelRegularizer.regularizer) == undefined) {
-			throw new Error("Invaild regularizer");
-		}
-		if (lambdaList.find((element) => element == layer.kernelRegularizer.lambda) == undefined) {
-			throw new Error("Invaild lambda");
-		}
-		if (kernelInitializerList.find((element) => element == layer.kernelInitializer) == undefined) {
-			throw new Error("Invaild kernelInitializer");
-		}
-	} catch (error) {
-		//send error to backend error log
-	}
-};
-
-const validateConv2D = (layer: any) => {
-	try {
-		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild kerenelSize");
-		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild filters");
-		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild strides");
-		if (activationList.find((element) => element == layer.activation) == undefined) {
-			throw new Error("Invaild Activation");
-		}
-		if (regularizerList.find((element) => element == layer.kernelRegularizer.regularizer) == undefined) {
-			throw new Error("Invaild regularizer");
-		}
-		if (lambdaList.find((element) => element == layer.kernelRegularizer.lambda) == undefined) {
-			throw new Error("Invaild lambda");
-		}
-		if (kernelInitializerList.find((element) => element == layer.kernelInitializer) == undefined) {
-			throw new Error("Invaild kernelInitializer");
-		}
-	} catch (error) {
-		//send error to backend error log
-	}
-};
-
-const validateMaxPooling2D = (layer: any) => {
-	//if pool size is invalid is could crash the backend
-	try {
-		const pool = layer.poolSize[0] * layer.poolSize[1];
-		const strides = layer.strides[0] * layer.strides[1];
-		if (pool > 100 || layer.units < 1) throw new Error("Invaild poolSize");
-		if (layer.units > 100 || layer.units < 1) throw new Error("Invaild poolSize");
-	} catch (error) {
-		//send error to backend error log
-	}
-};
-
-const validateFlatten = (layer: any) => {
-	try {
-		if (Object.keys(layer).length === 0) throw new Error("Invaild Flatten Layer");
-	} catch (error) {
-		//send error to backend error log
-	}
 };
---
+++ src/scripts/nnscripts.ts
@@ -0,0 +1,203 @@
+import * as tf from "@tensorflow/tfjs-node";
+
+//Interfaces
+export interface DenseLayer {
+	activation: string;
+	units: number;
+	kernelRegularizer: { regularizer: string; lambda: number } | any;
+	kernelInitializer: string;
+}
+export interface Conv2DLayer {
+	kernelSize: number; //window size, so if 5, then the window is 5x5
+	filters: number; //the number of kernelSize windows that is applied to the data
+	strides: number; //the step size of window!
+	activation: string;
+	kernelRegularizer: { regularizer: string; lambda: number } | any;
+	kernelInitializer: string;
+}
+
+export interface MaxPooling2D {
+	poolSize: number[];
+	strides: number[];
+}
+
+export interface Flatten {}
+
+export interface Layer {
+	type: string;
+	layer: DenseLayer | Conv2DLayer | MaxPooling2D | Flatten; //adding any to call properties without errors
+}
+
+export const MNISTShape = [28, 28, 1];
+
+//useful constants
+export const lossList = [
+	"meanSquaredError",
+	"meanAbsoluteError",
+	"meanAbsolutePercentageError",
+	"meanSquaredLogarithmicError",
+	"squaredHinge",
+	"hinge",
+	"categoricalHinge",
+	"logcosh",
+	"categoricalCrossentropy",
+	"sparseCategoricalCrossentropy",
+	"binaryCrossentropy",
+	"kullbackLeiblerDivergence",
+	"poisson",
+	"cosineProximity",
+];
+
+export const regularizerList = ["l1", "l2", "l1l2"];
+
+export const lambdaList = [0.0, 0.001, 0.01, 0.05, 0.1, 0.2, 0.3];
+
+export const activationList = [
+	"elu",
+	"hardSigmoid",
+	"linear",
+	"relu",
+	"relu6",
+	"selu",
+	"sigmoid",
+	"softmax",
+	"softplus",
+	"softsign",
+	"tanh",
+	"swish",
+	"mish",
+	"gelu",
+	"gelu_new",
+];
+
+export const kernelInitializerList = [
+	"constant",
+	"glorotNormal",
+	"glorotUniform",
+	"heNormal",
+	"heUniform",
+	"identity",
+	"leCunNormal",
+	"leCunUniform",
+	"ones",
+	"orthogonal",
+	"randomNormal",
+	"randomUniform",
+	"truncatedNormal",
+	"varianceScaling",
+	"zeros",
+];
+
+//helper functions
+export const convertLayers = (layers: any[]) => {
+	let fixedLayers;
+	try {
+		//first check if the layers object has been mannipulated
+		const layerCheck: Layer[] = layers;
+		//validate the layers data
+		fixedLayers = layers.map((layerObj, index) => {
+			switch (layerObj.type) {
+				case "Flatten":
+					validateFlatten(layerObj.layer);
+					return layerObj;
+				case "MaxPooling2D":
+					validateMaxPooling2D(layerObj.layer);
+					return layerObj;
+				case "Dense":
+					validateDense(layerObj.layer);
+					const copy: any = { ...layerObj };
+					copy.layer.kernelRegularizer = regularizerToFunction(copy.layer.kernelRegularizer);
+					return copy;
+				case "Conv2D":
+					validateConv2D(layerObj.layer);
+					const copy2: any = { ...layerObj };
+					copy2.layer.kernelRegularizer = regularizerToFunction(copy2.layer.kernelRegularizer);
+					return copy;
+				default:
+					throw new Error("Invaild Layer");
+			}
+		});
+	} catch (error) {
+		//here instead save it to an error log
+	}
+	return fixedLayers;
+};
+
+function regularizerToFunction(reg: { regularizer: string; lambda: number }) {
+	let regularizerFunction;
+	switch (reg.regularizer) {
+		case "l1":
+			regularizerFunction = tf.regularizers.l1({ l1: reg.lambda });
+			break;
+		case "l2":
+			regularizerFunction = tf.regularizers.l2({ l2: reg.lambda });
+			break;
+		case "l1l2":
+			regularizerFunction = tf.regularizers.l1l2({ l1: reg.lambda, l2: reg.lambda });
+			break;
+	}
+
+	return regularizerFunction;
+}
+
+const validateDense = (layer: any) => {
+	try {
+		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild Units");
+		if (activationList.find((element) => element == layer.activation) == undefined) {
+			throw new Error("Invaild Activation");
+		}
+		if (regularizerList.find((element) => element == layer.kernelRegularizer.regularizer) == undefined) {
+			throw new Error("Invaild regularizer");
+		}
+		if (lambdaList.find((element) => element == layer.kernelRegularizer.lambda) == undefined) {
+			throw new Error("Invaild lambda");
+		}
+		if (kernelInitializerList.find((element) => element == layer.kernelInitializer) == undefined) {
+			throw new Error("Invaild kernelInitializer");
+		}
+	} catch (error) {
+		//send error to backend error log
+	}
+};
+
+const validateConv2D = (layer: any) => {
+	try {
+		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild kerenelSize");
+		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild filters");
+		if (layer.units > 10 || layer.units < 1) throw new Error("Invaild strides");
+		if (activationList.find((element) => element == layer.activation) == undefined) {
+			throw new Error("Invaild Activation");
+		}
+		if (regularizerList.find((element) => element == layer.kernelRegularizer.regularizer) == undefined) {
+			throw new Error("Invaild regularizer");
+		}
+		if (lambdaList.find((element) => element == layer.kernelRegularizer.lambda) == undefined) {
+			throw new Error("Invaild lambda");
+		}
+		if (kernelInitializerList.find((element) => element == layer.kernelInitializer) == undefined) {
+			throw new Error("Invaild kernelInitializer");
+		}
+	} catch (error) {
+		//send error to backend error log
+	}
+};
+
+const validateMaxPooling2D = (layer: any) => {
+	//if pool size is invalid is could crash the backend
+	try {
+		const pool = layer.poolSize[0] * layer.poolSize[1];
+		const strides = layer.strides[0] * layer.strides[1];
+		if (pool > 100 || layer.units < 1) throw new Error("Invaild poolSize");
+		if (layer.units > 100 || layer.units < 1) throw new Error("Invaild poolSize");
+	} catch (error) {
+		//send error to backend error log
+	}
+};
+
+const validateFlatten = (layer: any) => {
+	try {
+		if (Object.keys(layer).length === 0) throw new Error("Invaild Flatten Layer");
+	} catch (error) {
+		//send error to backend error log
+	}
+};
